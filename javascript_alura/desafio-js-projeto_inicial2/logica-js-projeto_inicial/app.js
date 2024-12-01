c98618fdb386f9a0684049c513430080a20b6e4c
CTR54 <135168784+CTR54@users.noreply.github.com>
2024-12-01 08:41:34 -0300

    Update app.js

---
+++ javascript_alura/desafio-js-projeto_inicial2/logica-js-projeto_inicial/app.js
@@ -0,0 +1,15 @@
+alert("Boas vindas ao  nosso site!")
+let nome = "lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert("Erro!")
+let mensagemDeErro = 'Erro! preencha todos os campos'
+alert(mensagemDeErro)
+
+let nomeDeUsuario = prompt ("Qual seu nome? ");
+let idadeDeUsuario = prompt("Qual sua idade? ")
+
+if (idadeDeUsuario =< 18){
+    alert("Pode tirar a habilitação");
+}
