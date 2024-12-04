461c1262506d0ae817cefed1c06d0f600f3c1ba8
CTR54 <135168784+CTR54@users.noreply.github.com>
2024-12-03 21:13:18 -0300

    atualização

---
+++ desafio2/app.js
@@ -0,0 +1,10 @@
+let FimDeSemana = prompt("Que dia é hoje?")
+
+if (FimDeSemana == "sabado"){
+    alert("Bom fim de semana")
+} else if (FimDeSemana == "domingo"){
+    alert("Bom fim de semana")
+}
+ else{
+    alert("Tenha uma boa semana")
+}
