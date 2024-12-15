d2c3f319258a4f4af2f77565e665fb9129ab4b2e
CTR54 <135168784+CTR54@users.noreply.github.com>
2024-12-14 22:01:25 -0300

    atualização
    condições

--- desafio2/app.js
+++ desafio2/app.js
@@ -1,3 +1,4 @@
+
 let FimDeSemana = prompt("Que dia é hoje?")
 
 if (FimDeSemana == "sabado"){
@@ -11,8 +12,21 @@ if (FimDeSemana == "sabado"){
 
 let chuteNumerico = prompt("Digite um numero?")
 //erro no codigo
-if (chuteNumerico ==){
+if (chuteNumerico < 0){
     alert("O número digitado é negativo")
 } else{
     alert("O número digitado é positivo")
 }
+
+let pontuacao = prompt("digite sua pontuação")
+if (pontuacao >= 100) {
+    console.log('Parabéns, você venceu!');
+} else {
+    console.log('Tente novamente para ganhar.');
+}
+
+let saldoDaConta = 500;
+alert(`Óla, Seu saldo é de R$${saldoDaConta}`)
+
+let nomeDeUsuario = prompt("Digite seu nome: ")
+alert(`Óla, ${nomeDeUsuario} bem-vindo(a)`)
