127279a96e79b6087983e65021a361ac593f2d97
CTR54 <135168784+CTR54@users.noreply.github.com>
2025-01-02 22:04:04 -0300

    desafio contador
    solicite para que o usuário digite um número e faça o programa conta de 0 ate o número digitado.

---
+++ javascript_alura/desafio-js-projeto_inicial2/des003/app.js
@@ -0,0 +1,24 @@
+let contador = 1;
+while(contador <= 10){
+  console.log(contador);
+  contador++;
+} 
+
+let contadorRegresivo = 10;
+while(contadorRegresivo >= 0){
+    console.log(contadorRegresivo);
+    contadorRegresivo--;
+}
+
+let numeroRegresivo = prompt ("digite um número para a contagem regresiva");  
+while(numeroRegresivo >= 0){
+  console.log(numeroRegresivo);
+  numeroRegresivo--;
+}   
+
+let numeroProgresivo = prompt("digite um número para a contagem progresiva");
+let contador4 = 0;
+while( contador4 <= numeroProgresivo){
+  console.log(contador4);
+  contador4++;
+}
