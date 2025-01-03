e850ac1f1dda674dc398d96682478d430f68e8fa
CTR54 <135168784+CTR54@users.noreply.github.com>
2025-01-03 16:14:41 -0300

    criação da part03

--- javascript_alura/logica-js-projeto_inicial/app.js
+++ javascript_alura/logica-js-projeto_inicial/app.js
@@ -9,7 +9,7 @@ while(chute != numeroSecreto){
     chute = prompt("Escolha um número entre 1 e 10");
         // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    break;    
     } else{
         if(chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
@@ -21,3 +21,8 @@ while(chute != numeroSecreto){
         tentativas++;
     }
 }
+let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa';
+
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`)
+
+  
