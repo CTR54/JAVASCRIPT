fcd056fbce0301a5a57b9cf98055441234514889
CTR54 <135168784+CTR54@users.noreply.github.com>
2025-01-07 22:20:27 -0300

    atualização

--- javascript_alura/desafio-js-projeto_inicial2/desafio4/app.js
+++ javascript_alura/desafio-js-projeto_inicial2/desafio4/app.js
@@ -1,12 +1,13 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = parseInt(Math.random() * 10 + 1);
+let numeromaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeromaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto
 while(chute != numeroSecreto){
-    chute = prompt("Escolha um número entre 1 e 10");
+    chute = prompt(`Escolha um número entre 1 e ${numeromaximo}`);
         // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
     break;    
