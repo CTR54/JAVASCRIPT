ad4e9b0b82fd577ab1a61b9625b6c149ba468a6b
CTR54 <135168784+CTR54@users.noreply.github.com>
2025-01-23 19:26:15 -0300

    conclusão do desafio 2 do curso 2

--- js-alura-curso02/desafios/des02/app.js
+++ js-alura-curso02/desafios/des02/app.js
@@ -19,10 +19,28 @@ function dobroDoValorDigitado (){
 dobroDoValorDigitado();
 
 function calcularMedia() {
-    let nota1 = Number(prompt("Insira a primeira nota: "))
-    let nota2 = Number(prompt("Insira a segunda nota: "))
-    let nota3 = Number(prompt("Insira a terceira nota:"))
+    let nota1 = Number(prompt("Insira a primeira nota: "));
+    let nota2 = Number(prompt("Insira a segunda nota: "));
+    let nota3 = Number(prompt("Insira a terceira nota:"));
     let media = (nota1 + nota2 + nota3) / 3;
     alert(`A Media de suas notas é ${media}`);
 };
-calcularMedia()
+calcularMedia();
+
+function numeroMaior(){
+    let valor1 = Number(prompt("insira o primeiro valor: "));
+    let valor2 = Number(prompt("insira o segundo valor: "));
+    if(valor1 > valor2){
+        alert(`O numero ${valor1} e maior que ${valor2}`)
+    }else{
+        alert(`O numero ${valor2} e maior que ${valor1}`)
+    };
+};
+numeroMaior();
+
+function multiplicador(){
+    let numeroMultiplo = Number(prompt("digite um valor"));
+    let resultadoDaMultiplicacao = numeroMultiplo * numeroMultiplo;
+    alert(`O número ${numeroMultiplo} multiplicado por ele mesmo é ${resultadoDaMultiplicacao}`);
+};
+multiplicador();
