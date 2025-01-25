637a684fca83660b9295ee29c4a5dec9feff0ae2
CTR54 <135168784+CTR54@users.noreply.github.com>
2025-01-25 14:58:33 -0300

    dinamizando  frases  no jogo

--- js-alura-curso02/pt02/app.js
+++ js-alura-curso02/pt02/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
  function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -10,9 +11,21 @@ let numeroSecreto = gerarNumeroAleatorio();
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);    
-};
 
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa'
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+    } else{
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else{
+            exibirTextoNaTela('p', 'o número secreto é maior');
+        } 
+        tentativas++;
+    }
+}
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1);
 }
